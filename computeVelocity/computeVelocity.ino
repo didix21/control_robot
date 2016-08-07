2bc937c4ed6241cc56d035b3001e8f37a528b92f
Dídac Coll Pujals <Ddac Coll Pujals>
2016-08-07 02:08:15 +0200

    Second Update

--- computeVelocity/computeVelocity.ino
+++ computeVelocity/computeVelocity.ino
@@ -1,17 +1,25 @@
 #include "pins.h" 
 
-long m1, m2;
-long up_times[3]={0,0,0};
-int index = 0;
+long mA1, mA2, mB1, mB2;
+long up_times_A[3]={0,0,0},up_times_B[3]={0,0,0};
+int indexA = 0, indexB = 0;
 float speed = 0;
 int pwm = 0;
-long elapsedTime=0, tf=0, to=0;
+char dataComing= 0;
+bool motorAenabled = false, motorBenabled = false, firstTime = true;
 
-void encoderCaught(){// Interrupt
-  m1 = micros();
-  up_times[index]=m1-m2;
-  index=(index+1)%3
-  m2 = m1;
+void encMotorA(){// Interrupt Motor A
+  mA1 = micros();
+  up_times_A[indexA]=mA1-mA2;
+  indexA=(indexA+1)%3
+  mA2 = mA1;
+}
+
+void encMotorB(){// Interrupt Motor B
+  mB1 = micros();
+  up_times_B[indexB]=mB1-mB2;
+  indexB=(indexB+1)%3
+  mB2 = mB1;
 }
 
 void setup() {
@@ -23,15 +31,65 @@ void setup() {
   MYSERIAL.begin(BAUDRATE);
   pinMode(INTERRUPT_PIN,INPUT);
   digitalWrite(INTERRUPT_PIN,HIGH);
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN),encoderCaugh,RISING);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_MOTOR_A),encMotorA,RISING);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_MOTOR_B),encMotorB,RISING);
   to= millis();
+
+  MYSERIAL.println("This program computes the velocity of a motor,");
+  MYSERIAL.println("the program will print 100 values of velocities");
+  MYSERIAL.println();
+  MYSERIAL.print("Type any character to begin the program: ");
+  while(MYSERIAL.read()<0);
+  
+  
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+   if(firstTime){
+    MYSERIAL.println("Type A or B to take data from one of the motors: A");
+    while(dataComing == 0);
+    firstTime = false;
+   }
 
-  
-  
+  switch(dataComing){
+    case 'A':
+      digitalWrite(ENB,LOW);
+      digitalWrite(ENA,HIGH);
+      motorAenabled = true;
+    break;
+
+    case 'B':
+      digitalWrite(ENB,HIGH);
+      digitalWrite(ENA,LOW);
+      motorBenabled = true;
+    break;
+  }
+
+  if(motorAenabled && !motorBenabled){
+    analogWrite(IN2,0);
+    analogWrite(IN1,pwm);
+  }else if (!motorAenabled && motorBenabled){
+    analogWrite(IN4,0);
+    analogWrite(IN3,pwm);
+  }
+ 
+  while (i <= 260){
+    if(pwm < 255){
+      pwm = pwm + 1;
+    }
+    delay(100);
+    MYSERIAL.println(speed);
+    i++;
+  }
 
 }
+
+void serialEvent (){
+  while(MYSERIAL.available()){
+    dataComing=MYSERIAL.read();
+  }
+}
+
+
--- computeVelocity/pins.h
+++ computeVelocity/pins.h
@@ -1,6 +1,7 @@
 
 /* ==================== DEFINE PINS ==================== */
-#define INTERRUPT_PIN 2
+#define INTERRUPT_PIN_MOTOR_A 2
+#define INTERRUPT_PIN_MOTOR_B 3
 //Define PIN L298n
 #define ENA 22
 #define ENB 24
